bf28255207d0412ef053eca200788c2ce40a433d
Na21k <hulevych@hotmail.com>
2022-04-17 21:33:30 +0300

    Shopping list work

--- app/src/main/java/com/na21k/schedulenotes/ui/lists/shopping/ShoppingListActivity.java
+++ app/src/main/java/com/na21k/schedulenotes/ui/lists/shopping/ShoppingListActivity.java
@@ -1,16 +1,51 @@
 package com.na21k.schedulenotes.ui.lists.shopping;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
 
-import com.na21k.schedulenotes.R;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
-public class ShoppingListActivity extends AppCompatActivity {
+import com.na21k.schedulenotes.data.database.Lists.Shopping.ShoppingListItem;
+import com.na21k.schedulenotes.databinding.ActivityShoppingListBinding;
+
+public class ShoppingListActivity extends AppCompatActivity
+        implements ShoppingListAdapter.OnShoppingItemActionRequestedListener{
+
+    private ShoppingListViewModel mViewModel;
+    private ActivityShoppingListBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shopping_list);
+
+        mViewModel = new ViewModelProvider(this).get(ShoppingListViewModel.class);
+        mBinding = ActivityShoppingListBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        getWindow().setNavigationBarColor(Color.TRANSPARENT);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+    }
+
+    @Override
+    public void onShoppingItemUpdateRequested(ShoppingListItem item) {
+
+    }
+
+    @Override
+    public void onShoppingItemDeletionRequested(ShoppingListItem item) {
+
     }
 }
