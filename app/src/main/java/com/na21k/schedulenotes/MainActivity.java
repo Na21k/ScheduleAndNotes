5ed91b72302c08734393f13da3bcb68df4fb583d
Na21k <hulevych@hotmail.com>
2022-09-07 00:19:30 +0300

    Moved the settings button to the app bar

--- app/src/main/java/com/na21k/schedulenotes/MainActivity.java
+++ app/src/main/java/com/na21k/schedulenotes/MainActivity.java
@@ -1,7 +1,11 @@
 package com.na21k.schedulenotes;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,11 +18,11 @@ import androidx.work.ListenableWorker;
 import androidx.work.PeriodicWorkRequest;
 import androidx.work.WorkManager;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.na21k.schedulenotes.databinding.ActivityMainBinding;
 import com.na21k.schedulenotes.helpers.DateTimeHelper;
 import com.na21k.schedulenotes.helpers.NotificationsHelper;
 import com.na21k.schedulenotes.helpers.UiHelper;
+import com.na21k.schedulenotes.ui.settings.SettingsActivity;
 import com.na21k.schedulenotes.workers.MovieNotificationWorker;
 import com.na21k.schedulenotes.workers.MusicNotificationWorker;
 import com.na21k.schedulenotes.workers.WordOrPhraseNotificationWorker;
@@ -37,13 +41,11 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_schedule, R.id.navigation_notes, R.id.navigation_lists,
-                R.id.navigation_categories, R.id.navigation_settings/*, R.id.navigation_home*/)
-                .build();
+                R.id.navigation_categories/*, R.id.navigation_home*/).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
@@ -54,6 +56,24 @@ public class MainActivity extends AppCompatActivity {
 
         createNotificationChannels();
         ensureRecommendationsWorkerIsScheduled();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void ensureRecommendationsWorkerIsScheduled() {
---
+++ app/src/main/java/com/na21k/schedulenotes/ui/settings/SettingsActivity.java
@@ -0,0 +1,39 @@
+package com.na21k.schedulenotes.ui.settings;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.na21k.schedulenotes.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.settings, new SettingsFragment())
+                    .commit();
+        }
+
+        getWindow().setNavigationBarColor(Color.TRANSPARENT);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+    }
+}
