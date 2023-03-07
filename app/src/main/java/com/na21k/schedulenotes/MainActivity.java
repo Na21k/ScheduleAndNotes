690c5954056694d51345dc9938003eb3d6a74e5c
Na21k <hulevych@hotmail.com>
2023-03-07 04:16:58 +0200

    updated targetSdk to 33

--- app/src/main/java/com/na21k/schedulenotes/MainActivity.java
+++ app/src/main/java/com/na21k/schedulenotes/MainActivity.java
@@ -1,14 +1,21 @@
 package com.na21k.schedulenotes;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowCompat;
@@ -55,6 +62,8 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(mBinding.navView, navController);
 
         makeNavBarLookNice();
+
+        requestNotificationsPermission();
 
         createNotificationChannels();
         ensureRecommendationsWorkerIsScheduled();
@@ -154,5 +163,34 @@ public class MainActivity extends AppCompatActivity {
         NotificationsHelper.addMoviesNotificationChannel(this);
         NotificationsHelper.addMusicNotificationChannel(this);
         NotificationsHelper.addLanguagesListNotificationChannel(this);
+    }
+
+    private void requestNotificationsPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
+                ActivityCompat
+                        .checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityResultLauncher<String> requestNotificationsPermissionLauncher =
+                    registerForActivityResult(
+                            new ActivityResultContracts.RequestPermission(), isGranted -> {
+                            });
+
+            if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setIcon(R.drawable.ic_notifications_24);
+                builder.setTitle(R.string.notifications_permission_request_alert_title);
+                builder.setMessage(R.string.notifications_permission_request_alert_message);
+
+                builder.setPositiveButton(android.R.string.ok, (dialog, which) ->
+                        requestNotificationsPermissionLauncher
+                                .launch(Manifest.permission.POST_NOTIFICATIONS));
+
+                builder.show();
+            } else {
+                requestNotificationsPermissionLauncher
+                        .launch(Manifest.permission.POST_NOTIFICATIONS);
+            }
+        }
     }
 }
